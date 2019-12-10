bf951a4fa0b6d9be2b3777437fdacf51e57e67fe
rogerio-stubs <rostubs17@gmail.com>
2019-12-10 00:37:49 -0300

    primeira versão - itens básicos

---
+++ todo.js
@@ -0,0 +1,51 @@
+var listElement = document.querySelector('#app ul');
+var inputEelement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+    
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos()
+
+function addTodo() {
+    var todoText = inputEelement.value;
+
+    todos.push(todoText);
+    inputEelement.value = ' ';
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
