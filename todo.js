f3e34899fe44900e0f5d3475efcf1e3b318edc61
rogerio-stubs <rostubs17@gmail.com>
2021-02-16 04:24:46 -0300

    Adicionando/removendo itens

--- todo.js
+++ todo.js
@@ -2,17 +2,16 @@ const elementBtnSave = document.querySelector('.data-input button');
 const elementInputText = document.querySelector('.data-input input');
 const elementBtnDelete = document.querySelector('.clean button');
 
-var toDo = [];
 
 elementBtnSave.addEventListener('click', () => {
     const inputText = elementInputText.value;
     elementInputText.value = '';
     if(validation(inputText))
-        addToDo(inputText)
+        toDo(inputText)
 });
 
 elementBtnDelete.addEventListener('click', () => {
-    const node = document.querySelector('.done');
+    const node = document.querySelector('.table');
     if (node.parentNode)
         node.parentNode.removeChild(node);
 });
@@ -23,16 +22,24 @@ function validation(inputText) {
     alert('Informe um item!');
 }
 
-function addToDo(inputText) {
-    toDo.push(inputText);
+function toDo(inputText) {
     let li = document.createElement('li');
     let input = document.createElement('input');
     let p = document.createElement('p');
 
     let ul = document.querySelector('.to-do ul');
     li.appendChild(input);
-    input.setAttribute('type', 'checkbox')
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('onclick', 'done()');
     li.appendChild(p);
     p.textContent = inputText;
+    ul.appendChild(li);
+}
+
+function done() {
+    let li = document.createElement('li');
+    li.textContent = 'texto';
+
+    let ul = document.querySelector('.table');
     ul.appendChild(li);
 }
