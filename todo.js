31020c02fb80b22d4b9373468886f1901df709eb
rogerio-stubs <rostubs17@gmail.com>
2021-03-07 13:27:30 -0300

    Incluindo elementos com a tecla enter

--- todo.js
+++ todo.js
@@ -3,21 +3,32 @@ const elementInputText = document.querySelector('.data-input input');
 const elementBtnDelete = document.querySelector('.clean button');
 
 elementBtnSave.addEventListener('click', () => {
-    // adicionar elemento com o enter
-    const inputText = elementInputText.value;
-    elementInputText.value = '';
-    if (validation(inputText))
-        toDo(inputText)
+    validation(returnValue())
 });
 
+$(document).keypress((e) => {
+    if (e.which == 13){
+        e.preventDefault();
+        validation(returnValue())
+    }
+});
+
+function returnValue() {
+    const inputText = elementInputText.value;
+    elementInputText.value = '';
+    return inputText;
+}
+
 function validation(inputText) {
-    if (!inputText == "" && !(inputText.match(/^\s+$/)))
-        return true;
-    alert('Informe um item!');
+    if (!inputText == "" && !(inputText.match(/^\s+$/))){
+        toDo(inputText) 
+    }
+    else {
+        alert('Informe um item!');
+    }
 }
 
 function toDo(inputText) {
-    // implementar a quebra de liha
     let li = document.createElement('li');
     let input = document.createElement('input');
     let p = document.createElement('p');
